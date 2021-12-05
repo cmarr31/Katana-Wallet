aea2f24eac055f8e6822a2f4f63399252eebeacc
AntimonyIQ <antimonyiq@gmail.com>
2021-12-05 16:15:05 +0800

    Add project files.

---
+++ Katana Wallet/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Katana_Wallet
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
